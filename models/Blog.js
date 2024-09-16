afbd0070a0534d216390248bb1012b51839f2913
purniiiima <purniiiima@gmail.com>
2024-09-16 16:59:13 +0530

    updated field

--- models/Blog.js
+++ models/Blog.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const BlogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   category: { type: String, default: 'Uncategorized' },
-  description: { type: String, required: true },
+  // description: { type: String, required: true },
   thumbnail: { type: String }, 
   name: { type: String, required: true },
   role: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+
+},{ timestamps: true });
 
 module.exports = mongoose.model('Blog', BlogSchema);
