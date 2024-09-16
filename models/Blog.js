0783e0c681f86a0cddcc0cb16c8a7879a9d0b655
purniiiima <purniiiima@gmail.com>
2024-09-16 14:15:13 +0530

    updated field

--- models/Blog.js
+++ models/Blog.js
@@ -4,7 +4,7 @@ const BlogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   category: { type: String, default: 'Uncategorized' },
   description: { type: String, required: true },
-  thumbnail: { type: String }, // Thumbnail image file path
+  thumbnail: { type: String }, 
   name: { type: String, required: true },
   role: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
